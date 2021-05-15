ab42d91d1dc1cae29c3e2759f37b5b8b46c96b35
Veljko <veljkos82@gmail.com>
2021-05-16 01:14:42 +0200

    Added additional validation for shifts when creating calendar. Must have exact number of shifts compared to days in selected month where numbers one and two are shifts, while non working days are marked as zeroes. Also added validation if calendar exists and added custom messages for check if main heading exists, is number of shifts same as number of months days and for calendar itself validation errors.

--- app/Http/Controllers/API/CalendarController.php
+++ app/Http/Controllers/API/CalendarController.php
@@ -84,6 +84,18 @@ class CalendarController extends Controller
             $validator = Validator::make($request->all(), [
                 "mh_id" => "required|integer",
                 "employee_shifts" => "required|array",
+                "employee_shifts.*.employee_id" => "required|integer",
+                "employee_shifts.*.shifts" => "required|array",
+                "employee_shifts.*.shifts.*" => [
+                    "required",
+                    "integer",
+                    function ($attribute, $value, $fail){
+                        //Nulte vrednosti u smeni radnika su neradni dani.
+                        if($value !== 1 && $value !== 2 && $value !== 0){
+                            $fail('The '.$attribute.' is invalid.');
+                        }
+                    }
+                ],
             ]);
 
             if($validator->fails()){
@@ -113,64 +125,66 @@ class CalendarController extends Controller
                     
                 }
 
-                $cal = null;
+                //First check. Check if calendar already exists.
+                $checkIfcalExists = Calendar::where("mh_id", "=", $request->mh_id)->first();
 
-                if($cal){
+                //Second check.
+                $arr1 = [];//Checks if entered employee shifts exceeds or doesn't have enough days in month.
+                //If days for any employee doesn't check out, it'll show an error and resulted calendar wouldn't be stored.
+                for($i=0;$i<count($request->employee_shifts);$i++){
+
+                    if(count($request->employee_shifts[$i]["shifts"]) != count($obj->dayNamesInMonthWithNums)){
+                        //Logs an error.
+                        $arr1[$i] = new stdClass();
+                        $arr1[$i]->message1 = "Employee with id ".$request->employee_shifts[$i]["employee_id"]." doesn't have proper quantity of shifts";
+                        $arr1[$i]->message2 = "Quantity needed ".count($obj->dayNamesInMonthWithNums).", quanity had ".count($request->employee_shifts[$i]["shifts"]).".";
+                    
+                    }
+
+                }
+
+                $checkShifts = count($arr1) > 0 ? true : false;
+
+                if($checkIfcalExists || $checkShifts==true){
+
+                    $errors = [];
+                    if($checkShifts==true){
+
+                        $errors[0] = new stdClass();
+                        $errors[0]->message = "Shift placements aren't Ok!";
+                        $errors[0]->List_of_employees_that_dont_have_proper_shifts = $arr1;
+
+                    }
+
+                    if($checkIfcalExists){
+                        $errors[1] = new stdClass();
+                        $errors[1]->message = "Calendar already exists!";
+                    }
 
                     $response = array(
-                        "message" => "Calendar with main_heading id = ".$request->mh_id." already exists!",
+                        count($errors)>1 ? "Errors" : "Error " => $errors,
                     );
-    
-                    return response($response, 200);
 
+                    return response($response, 200);
+                    
                 }
                 else{
 
-                    $arrCheck = [];//Checks if entered employee shifts exceeds days in month.
-                    //If days for any employee doesn't check out, it'll show an error and resulted calendar wouldn't be stored.
-                    for($i=0;$i<count($request->employee_shifts);$i++){
+                    $calendar = new Calendar;
+                    $calendar->employee_shifts = $request->employee_shifts;
+                    $calendar->daynums = $obj->dayNamesInMonthWithNums;
+                    $calendar->user_id = auth()->user()->id;
+                    $calendar->mh_id = $request->mh_id;
 
-                        if(count($request->employee_shifts[$i]["shifts"]) >= count($obj->dayNamesInMonthWithNums)){
-                            $arrCheck[$i] = true;
-                        }
-                        else{
-                            $arrCheck[$i] = false;
-                        }
+                    $calendar->save();
 
-                    }
+                    $cal = Calendar::with("user", "main_heading")->findOrFail($calendar->id);
+                    $response = array(
+                        "message" => "You created a calendar!",
+                        "cal" => $cal
+                    );
 
-                    $checkSfits = array_reduce($arrCheck, function (bool $acc, $value) {
-                        return !$acc ? $acc : $value === null;
-                    }, true);
-
-                    if($checkSfits === false){
-
-                        $calendar = new Calendar;
-                        $calendar->employee_shifts = $request->employee_shifts;
-                        $calendar->daynums = $obj->dayNamesInMonthWithNums;
-                        $calendar->user_id = auth()->user()->id;
-                        $calendar->mh_id = $request->mh_id;
-                        $calendar->save();
-
-                        $cal = Calendar::with("user", "main_heading")->findOrFail($calendar->id);
-                        $response = array(
-                            "message" => "You created a calendar!",
-                            "cal" => $cal
-                        );
-
-                        return response($response, 200);
-
-                    }
-                    else{
-
-                        $response = array(
-                            "message" => "Shift placements aren't Ok!",
-                            "cal" => $cal
-                        );
-
-                        return response($response, 200);
-
-                    }
+                    return response($response, 200);
 
                 }
 
